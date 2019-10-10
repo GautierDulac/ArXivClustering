00dc47060aa27a81d609f2c07ab49e6eafdb60d7
GD <gautier.dulac@polytechnique.Edu>
2019-10-10 16:02:51 +0200

    Adapt scrapper for arxiv website few econometrics and finance categories

--- arxivscrapper.py
+++ arxivscrapper.py
@@ -21,18 +21,17 @@ def main_arxivscrapper(run_loading=False):
     if run_loading:
         chunk_size = 50
         # ARXIV QUERY
-        queries = ['econ']
+        queries = ['cat:econ.EM', 'cat:econ.TH', 'econ.GN', 'q-fin.ST', 'q-fin.TR', 'q-fin.EC']
         start = time.time()
-        with open('arxiv-paper-titles-data-abstracts.csv', mode='w') as data_file:
+        with open('arxiv-paper-titles-data-abstracts.csv', mode='w', newline="") as data_file:
             file_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for query in queries:
+                print("Category in process : " + query)
                 num = 0
-                for chunk_i in range(0, 20000, chunk_size):
+                for chunk_i in range(0, 2000, chunk_size):
                     feed = feedparser.parse(
                         'http://export.arxiv.org/api/query?search_query=%s&start=%d&max_results=%d' % \
                         (query, chunk_i, chunk_size))
-                    print(feed)
-
                     for i in range(len(feed.entries)):
                         entry = feed.entries[i]
                         article_id = entry.id
@@ -43,6 +42,7 @@ def main_arxivscrapper(run_loading=False):
                         file_writer.writerow([unidecode(article_id), unidecode(title), unidecode(published),
                                               unidecode(author), unidecode(abstract), str(0)])
                         num = num + 1
+                    print(str(num) + " article : " + title)
                 print('%d titles saved from Arxiv %s.' % (num, query))
 
         end = time.time()
